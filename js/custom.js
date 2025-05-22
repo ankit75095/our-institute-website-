fd04aeaac35bb60573442ad7bbe9789683768a62
learning8435 <sonulodhi843550@gmail.com>
2025-05-22 15:10:16 +0530

    change

--- js/custom.js
+++ js/custom.js
@@ -181,48 +181,48 @@ $(document).ready(function () {
 
 
 
-$(document).ready(function () {
-$('.placement-slider').slick({
-  infinite: true,
-  speed: 2000,
-  slidesToShow: 4,
-  slidesToScroll: 1,
-  autoplay: true,
-  autoplaySpeed: 0,
-  arrows: false,
-  cssEase: 'linear',
+// $(document).ready(function () {
+// $('.placement-slider').slick({
+//   infinite: true,
+//   speed: 2000,
+//   slidesToShow: 4,
+//   slidesToScroll: 1,
+//   autoplay: true,
+//   autoplaySpeed: 0,
+//   arrows: false,
+//   cssEase: 'linear',
   
 
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        adaptiveHeight: true,
+//   responsive: [
+//     {
+//       breakpoint: 1024,
+//       settings: {
+//         slidesToShow: 3,
+//         slidesToScroll: 1,
+//         adaptiveHeight: true,
 
-      }
-    },
-    {
-      breakpoint: 768,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1,
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        speed: 1500,
-        adaptiveHeight: true,
+//       }
+//     },
+//     {
+//       breakpoint: 768,
+//       settings: {
+//         slidesToShow: 2,
+//         slidesToScroll: 1,
+//       }
+//     },
+//     {
+//       breakpoint: 480,
+//       settings: {
+//         slidesToShow: 1,
+//         slidesToScroll: 1,
+//         speed: 1500,
+//         adaptiveHeight: true,
 
-      }
-    }
-  ]
-});
-});
+//       }
+//     }
+//   ]
+// });
+// });
 
 
 
